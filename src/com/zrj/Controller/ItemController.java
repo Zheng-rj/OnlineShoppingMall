a561f5e9c481d04df837951ba43eac47b679162d
Zheng-rj <Zheng-rj@github.com>
2019-12-25 08:44:32 +0800

    使用非注解的处理器映射器和适配器 一个contorller只能配置一个方法

--- src/com/zrj/Controller/ItemController.java
+++ src/com/zrj/Controller/ItemController.java
@@ -1,37 +1,40 @@
 package com.zrj.Controller;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
 import com.zrj.po.Item;
 
-public class ItemController implements Controller{
+public class ItemController implements HttpRequestHandler {
 
 	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		List<Item> items = new ArrayList<Item>();
+	public void handleRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		List<Item> list = new ArrayList<Item>();
 		
 		Item i1 = new Item();
-		i1.setItemName("cocaCola");
-		i1.setPrice(3F);
-		
 		Item i2 = new Item();
+		
+		i1.setItemName("cocaCola");
+		i1.setPrice(3f);
 		i2.setItemName("MI6");
-		i2.setPrice(2600F);
+		i2.setPrice(2600f);
 		
-		items.add(i1);
-		items.add(i2);
+		list.add(i1);
+		list.add(i2);
 		
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.addObject("items", items);
-		modelAndView.setViewName("WEB-INF/jsp/List.jsp");
-		return modelAndView;
+		request.setAttribute("items", list);
+		request.getRequestDispatcher("WEB-INF/jsp/List.jsp").forward(request, response);
 	}
 
 
